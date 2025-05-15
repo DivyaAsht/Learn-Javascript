3fa4c372c353b08ad7c0c5e2aa9a0c4299299e17
Divya Ashtikar <112276343+DivyaAsht@users.noreply.github.com>
2025-05-15 21:58:43 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,36 @@
+/*let age=18;
+let resut=age>=18?"adult":"not adult";
+console.log(resut);
+
+const expr="papaya";
+switch(expr){
+    case'orange':
+    console.log("orange color");
+    break;
+    case'apple':
+    console.log("red color");
+    break;
+    case 'papaya':
+    console.log("yellow");
+    break;
+    default:
+        console.log("sorry!! not ",expr);    
+}
+
+let num=prompt("enter a number : ");
+if(num%2===0){
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
+
+const marks=prompt("enter marks : ");
+if(marks<=100 && marks>90)
+    console.log("A grade");
+else if(marks<=90 && marks>80)
+    console.log("B grade");
+else if(marks<=80 && marks>70)
+    console.log("C grade");
+else
+console.log("Fail!!!!");*/
