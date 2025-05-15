87ea5857dc768b5ad07f8a601af9ee20ac62b667
Divya Ashtikar <112276343+DivyaAsht@users.noreply.github.com>
2025-05-15 21:53:46 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+const student={
+    name:"rahul",
+    age:20,
+    ispass:true,
+    cgpa:8.2
+};
+console.log(student);
+console.log(student.name);
+console.log(student["age"]);
+console.log(student["age"]=student["age"]+1);
